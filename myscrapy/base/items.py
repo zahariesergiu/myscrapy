bbdcb9774b5005c5284da2dbef115280d26a0c7e
Sergiu <sergiu.zaharie@united-academics.org>
2017-09-16 14:52:45 +0300

    Add field default value feature to BaseItem

--- myscrapy/base/items.py
+++ myscrapy/base/items.py
@@ -1,10 +1,19 @@
 import scrapy
+import uuid as uuid
 
 
 class BaseItem(scrapy.Item):
     """
     Base Item class for this project, includes fields that all Items should have, defined by the project goal.
     """
+
+    def __init__(self, *args, **kwargs):
+        super(BaseItem, self).__init__(*args, **kwargs)
+
+        # set default value of a field
+        for field in self.fields:
+            if 'default' in self.fields[field]:
+                self[field] = self.fields[field]['default']
 
     uuid = scrapy.Field()
     source_url = scrapy.Field()
