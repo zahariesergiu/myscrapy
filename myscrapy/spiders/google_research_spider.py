21153e788c0a549da6ee835216f8b038ae8eee13
Sergiu <sergiu.zaharie@united-academics.org>
2017-09-18 11:44:39 +0300

    Add GoogleResearchSpider.parse_publications_research_areas method

--- myscrapy/spiders/google_research_spider.py
+++ myscrapy/spiders/google_research_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from .. import utils
+
 
 class GoogleResearchSpider(scrapy.Spider):
     """Spider which collects items about Google research papers, authors, downloads document files and image files"""
@@ -12,7 +14,25 @@ class GoogleResearchSpider(scrapy.Spider):
             yield scrapy.Request(url=start_url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        return self.parse_publications_research_areas(response)
+
+    def parse_publications_research_areas(self, response):
+        """Parse publications page to extract research area URLs and make requests to those pages"""
+
+        research_area_urls = scrapy.Selector(text=response.body).xpath('//*[@id="maia-main"]/div[3]/div[2]/div['
+                                                                       '1]/div/ul//li/a/@href').extract()
+        # Correct URLs, add domain
+        domain = utils.get_domain(response)
+        research_area_urls = [domain + url for url in research_area_urls]
+
+        # Yield requests with parsed research area URLs
+        for research_area_url in research_area_urls:
+            yield scrapy.Request(url=research_area_url, callback=self.parse_research_area)
+            break  # break after first yield, use in dev to scrape just 1 item
+
+    def parse_research_area(self, response):
+        """Parse research area page to extract paper items (incomplete paper item) + their PDFs  and/or paper URLs and
+        author items"""
 
 
 class GoogleResearchAuthorItem(scrapy.Item):
